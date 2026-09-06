9e11d7f312d69db6c8513917e8fc8f2f78125b0e
Matthias Steiner <m@kjpjm.de>
2026-09-06 12:08:54 +0200

    fix datatype of font weight

--- style/font.ts
+++ style/font.ts
@@ -57,15 +57,15 @@ const style = {
   italic: "italic",
 };
 const weight = {
-  thin: 100, // Thin
-  extralight: 200, // Extra Light
-  light: 300, // Light
-  normal: 400, // Normal
-  medium: 500, // Medium
-  semibold: 600, // Semi-bold
-  bold: 700, // Bold
-  extrabold: 800, // Extra Bold
-  black: 900, // Black
+  thin: "100", // Thin
+  extralight: "200", // Extra Light
+  light: "300", // Light
+  normal: "400", // Normal
+  medium: "500", // Medium
+  semibold: "600", // Semi-bold
+  bold: "700", // Bold
+  extrabold: "800", // Extra Bold
+  black: "900", // Black
 };
 
 export const font = {
